9ce19cc5b4fe57b90f73f46f701345e216129be7
tutu <391476634@qq.com>
2018-01-11 22:13:20 +0800

    fix

---
+++ Helper/TextUIHelper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Gj
+{
+    public class TextUIHelper : MonoBehaviour
+    {
+        public string key = " ";
+        void Start()
+        {
+            GetComponent<Text>().text = Localization.GetInstance.GetValue(key);
+        }
+    }
+}
